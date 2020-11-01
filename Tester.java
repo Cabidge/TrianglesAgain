857bc363725c7740692d4fa85a401f14c429c91b
wdong20 <wdong20@stuy.edu>
2020-11-01 14:05:33 -0500

    Created Tester class. Point class tested and working.

---
+++ Tester.java
@@ -0,0 +1,9 @@
+public class Tester {
+    public static void main(String[] ayHowsItGoinHomies) {
+        Point p = new Point(23.4,-333);
+
+        System.out.println("The following must all print out \"true\" or else I made a mistake.");
+        System.out.println(p.getX() == 23.4);
+        System.out.println(p.getY() == -333);
+    }
+}
