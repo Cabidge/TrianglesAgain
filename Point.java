c201e74c8779f16f5dc900d77b7b5acbba85420f
wdong20 <wdong20@stuy.edu>
2020-11-01 14:09:25 -0500

    Created and tested the Point class distanceTo method.

--- Point.java
+++ Point.java
@@ -13,4 +13,10 @@ public class Point {
     public double getY() {
         return y;
     }
+
+    public double distanceTo(Point other) {
+        double x = this.x - other.x;
+        double y = this.y - other.y;
+        return Math.sqrt(x * x + y * y);
+    }
 }
--- Tester.java
+++ Tester.java
@@ -2,8 +2,13 @@ public class Tester {
     public static void main(String[] ayHowsItGoinHomies) {
         Point p = new Point(23.4,-333);
 
+        // Distance between the two should be 5
+        Point a = new Point(3,4);
+        Point b = new Point(6,8);
+
         System.out.println("The following must all print out \"true\" or else I made a mistake.");
         System.out.println(p.getX() == 23.4);
         System.out.println(p.getY() == -333);
+        System.out.println(a.distanceTo(b) == 5);
     }
 }
